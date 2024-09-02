97366eb77c3575e571fa841597a0fb6b7af34a08
Zhendong Tian <zhendong.tian96@gmail.com>
2024-09-02 20:52:24 +0100

    first commit

---
+++ server/routers/user.ts
@@ -0,0 +1,20 @@
+import {z} from 'zod';
+import { router, procedure } from "../trpc";
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export const userRouter = router({
+    getUsers: procedure.query(async ()=> {
+        return await prisma.user.findMany();
+    }),
+
+    addUser: procedure.input(z.object({name: z.string(), race: z.string()})).mutation(async (opts) => {
+        const { input } = opts;
+        await prisma.user.create({
+            data: {
+                name: input.name,
+                race: input.race
+            }
+        });
+    })
+});
---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,25 @@
+import { CONFIG } from 'src/config-global';
+import { DashboardLayout } from 'src/layouts/dashboard';
+
+import { AuthGuard } from 'src/auth/guard';
+import { TRPCProvider } from 'server/client';
+// ----------------------------------------------------------------------
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  if (CONFIG.auth.skip) {
+    return <DashboardLayout>{children}</DashboardLayout>;
+  }
+
+  return (
+    <AuthGuard>
+      <TRPCProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+      </TRPCProvider>
+
+    </AuthGuard>
+  );
+}
